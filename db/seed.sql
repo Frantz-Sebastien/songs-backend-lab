3f97b1d045c29acfa0a8af45c0975d241708de27
Frantz-Sebastien Mathias <frantz-sebastienmathias@pursuit.org>
2023-07-23 08:17:42 -0400

    index route complete. I think

---
+++ db/seed.sql
@@ -0,0 +1,5 @@
+\c songs_dev;
+
+INSERT INTO songs (name, artist, album, time, is_favorite) VALUES
+('Wamuhle', 'Slade', 'My Turn', '7:33', true),
+('Yellow', 'Coldplay', 'Parachutes', '4:32', false);
